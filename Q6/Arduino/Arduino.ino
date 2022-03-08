6296afa6465502c6b894ace1b5d687b4f227dab6
Radin Shayanfar <radin.shayanfar@gmail.com>
2022-03-09 00:37:46 +0330

    Q6 code added.

---
+++ Q6/Arduino/Arduino.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Mohammaderfan Ghasemi    --  9731048");
+  Serial.println("Radin         Shayanfar  --  9731032");
+  delay(800);
+}
